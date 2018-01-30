4190f2b7c8fc3ba556bb626578837f97f25ac586
iriscsun <iris.sun@nike.com>
2018-01-30 11:04:42 -0800

    test

--- exercise-wk2/exercise1.R
+++ exercise-wk2/exercise1.R
@@ -1,3 +1,8 @@
+library(stringr)
+
+
+
+
 ## Part 1: Debugging
 
 my.num <- 6
@@ -28,26 +33,94 @@ said.the.famous <- paste(my.line, " - ", initial)
 
 ## Part 2 - Vector and function practice
 
+######
 # Make a vector and use typeof to check what type R considers it to be
 
+typeof(c("dogs", "cats", "ferrets???"))  # "character"
+typeof(c(1, 4, 6))  # "double"
+typeof(c(1, 2, "dogs"))  # "character"
+
+
+######
 # Write a function `CompareLength` that takes in 2 vectors, and returns the sentence:
 # "The difference in lengths is N"
 
+CompareLength <- function(v1, v2) {
+  diff <- abs(length(v1) - length(v2))
+  phrase <- paste("The difference in lengths is", diff)
+  return(phrase)
+}
 
+vector1 <- c(1, 2, 3, 4, 5)
+vector2 <- c(1, 2, 3)
+
+print(CompareLength(vector1, vector2))
+
+
+
+######
 # Pass two vectors of different length to your `CompareLength` function
 
+# v1 <- c(1, 2, 3, 4, 5)
+# v2 <- c(1, 2, 3)
 
+
+
+
+
+
+
+
+######
 # Write a function `DescribeDifference` that will return one of the following statements:
 # "Your first vector is longer by N elements"
 # "Your second vector is longer by N elements"
 
+
+
+
+
+
+
+
+
+######
 # Pass two vectors to your `DescribeDifference` function
 
+# v1 <- c(1:15)
+# v2 <- c(9:17)
+# v.diff <- DescribeDifference(v2, v1)
+# print(v.diff)
 
-# Write a function `CombineVectors` that takes in 3 vectors and combines them into one
 
-# Send 3 vectors to your function to test it.
 
-# Write a function `CapsTime` that takes in a vector of names of courses you're taking, ex "Informatics 201" and removes all capital letters
+
+
+
+
+# DescribeDifference <- function(a,b) {
+#   diff <- length(a) - length(b)
+#   if(diff > 0) {
+#     sentence <- paste('Your first vector is longer by', diff, 'elements')
+#   } else {
+#     sentence <- paste('Your second vector is longer by', -diff, 'elements')
+#   }
+#   return(sentence)
+# }
+
+
+######
+
+# CompareLength <- function(v1, v2) {
+#   diff <- length(v1) - length(v2)
+#   abs.diff <- abs(diff)
+#   diff.string <- paste('The difference in lengths is', abs.diff)
+#   return(diff.string)
+# }
+
+
+
+
+
 
 
